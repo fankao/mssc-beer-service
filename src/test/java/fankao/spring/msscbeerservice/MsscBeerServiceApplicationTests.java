ea5eff1f52499f25ecca6f27fe9085879caf1f96
fankao <minhchiendc@gmail.com>
2021-05-02 14:51:34 +0700

    init new project. Closed #1

---
+++ src/test/java/fankao/spring/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fankao.spring.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
